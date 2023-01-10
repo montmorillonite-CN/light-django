59df42146df585f91e60b81ca7392ce1dadb1580
一块蒙脱石 <104210832+montmorillonite-CN@users.noreply.github.com>
2023-01-10 20:33:48 +0800

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,10 @@
+# configs for changing between dev. & prod.
+DEBUG = True
+# ALLOWED_HOSTS = ["127.0.0.1"]
+
+# locale configs, take `Chinese` as an example.
+LANGUAGE_CODE = "zh-hans"
+TIME_ZONE = "UTC+8"
+
+# WARNING: configs below should NOT be changed.
+ROOT_URLCONF = "urls"
