de5b0a908f1ce58a539151897315be9f3b59d6f9
一块蒙脱石 <104210832+montmorillonite-CN@users.noreply.github.com>
2023-01-11 12:38:42 +0800

    Add files via upload

--- settings.py
+++ settings.py
@@ -2,6 +2,9 @@
 DEBUG = True
 ALLOWED_HOSTS = ["127.0.0.1"]
 
+# secret_key, do NOT let others knows.
+SECRET_KEY = "1145141919810"
+
 # locale configs, take `Chinese` as an example.
 LANGUAGE_CODE = "zh-hans"
 TIME_ZONE = "UTC+8"
@@ -9,4 +12,4 @@ TIME_ZONE = "UTC+8"
 # WARNING: configs below should NOT be changed
 #          unless you know what are you doing.
 ROOT_URLCONF = "urls"
-__WIBSITE__ = "website"
+WEBSITE_FOLDER = "website"
